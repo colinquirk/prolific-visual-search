28aa92878a54313dfa16d8379c4a6b4627a1b29e
Colin Quirk <cquirk@uchicago.edu>
2020-05-11 17:03:24 -0500

    basic app

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-import flask
+from flask import Flask, redirect, render_template, url_for
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return ""
+    return redirect(url_for('consent'))
+
+
+@app.route('/consent', methods=['GET'])
+def consent():
+    return render_template('consent.html')
+
+
+@app.route('/consent-failure', methods=['GET'])
+def consent_failuer():
+    return render_template('consent-failure.html')
+
+
+@app.route('/experiment', methods=['GET', 'PUT'])
+def experiment():
+    return render_template('experiment.html')
+
 
 app.run()
