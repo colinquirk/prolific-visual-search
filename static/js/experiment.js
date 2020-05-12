6c586e2354944848263bd0b759451a32aca69a79
Colin Quirk <cquirk@uchicago.edu>
2020-05-12 12:16:08 -0500

    Add basic instructions

--- static/js/experiment.js
+++ static/js/experiment.js
@@ -1,9 +1,58 @@
-function main() {
-    var canvas = document.getElementById("experiment");
-    var ctx = canvas.getContext("2d");
-    
-    ctx.font = "20px Roboto";
-    ctx.fillText("Test", 100, 100)
+function setup() {
+    window.canvas = document.getElementById("experiment");
+    window.ctx = canvas.getContext("2d");
+    window.ctx.font = "24px Roboto";
+    window.ctx.textAlign = "center";
 }
 
-$(document).ready(main);
+function clearCanvas() {
+    window.ctx.clearRect(0, 0, window.canvas.width, window.canvas.height);
+}
+
+function writeCenterText(text) {
+    ctx.fillText(text, window.canvas.width / 2, window.canvas.height / 2);
+}
+
+function welcome() {
+    writeCenterText('Welcome to the experiment!\nPress the space bar to begin.');
+}
+
+function instructText() {
+    writeCenterText('You are searching for Ts');
+}
+
+function startExperiment() {
+    writeCenterText('Press space to start the experiment.');
+}
+
+instruct_states = [
+    welcome,
+    instructText,
+    startExperiment
+]
+
+function instructions() {
+    state = 0;
+    instruct_states[state]();
+
+    $(document).keypress(function(e){
+        console.log('space')
+        if (e.keyCode == 32) {
+            state += 1;
+            if (state >= instruct_states.length) {
+                clearCanvas();
+                $(document).off();
+            } else {
+                clearCanvas();
+                instruct_states[state]();
+            }
+        }
+    })
+}
+
+function main() {
+    setup();
+    instructions();
+}
+
+$(window).on('load', main);
